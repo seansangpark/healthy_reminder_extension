d9f0a8716f8d38176a200bb6ecda5ea2b4fa585a
Sean Park <seansangpark@gmail.com>
2021-08-29 22:06:23 -0700

    Basic timer functionality implemented

--- background.js
+++ background.js
@@ -4,7 +4,31 @@ chrome.alarms.create('reminderTimer', {
 
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === 'reminderTimer') {
+    chrome.storage.local.get(['timer', 'isRunning', 'timeOption'], (res) => {
+      if (res.isRunning) {
+        let timer = res.timer + 1;
+        let isRunning = true;
+        if (timer === 60 * res.timeOption) {
+          this.registration.showNotification('Reminder Timer', {
+            body: `${res.timeOption} minutes has passed!`,
+            icon: 'icon.png',
+          });
+          timer = 0;
+          isRunning = false;
+        }
+        chrome.storage.local.set({
+          timer,
+          isRunning,
+        });
+      }
+    });
   }
 });
 
-chrome.storage.local.get(['timer', 'isRunning']);
+chrome.storage.local.get(['timer', 'isRunning', 'timeOption'], (res) => {
+  chrome.storage.local.set({
+    timer: 'timer' in res ? res.timer : 0,
+    isRunning: 'isRunning' in res ? res.isRunning : false,
+    timeOption: 'timeOption' in res ? res.timeOption : 25,
+  });
+});
